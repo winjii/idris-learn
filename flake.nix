5a82027325bc249a7dbc991a2cbd2af8ddfd0b67
winjii <wing3196@gmail.com>
2024-01-20 20:18:20 +0900

    initialize `hello` package

--- flake.nix
+++ flake.nix
@@ -3,36 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.05-darwin";
-    idr2nix.url = "git+https://git.sr.ht/~thatonelutenist/idr2nix?ref=trunk";
-    idr2nix.inputs.nixpkgs.follow = "nixpkgs";
-    idris.url = "github:idris-lang/Idris2";
-    idirs.inputs.nixpkgs.follow = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils/main";
+    idr2nix.url = "git+https://git.sr.ht/~thatonelutenist/idr2nix?ref=trunk";
+    idr2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, idris, ... }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-
-      outputs-overlay = system: pkgs: prev: {
-        idris-playground-shell = pkgs.mkShell {
-          nativeBuildInputs = [
-            idris.packages."${system}".default
-          ];
-        };
-      };
-    in
-    flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (outputs-overlay system) ];
-        };
-      in
-      {
-        devShells = {
-          default = pkgs.idris-playground-shell;
-        };
-      }
-    );
+  outputs = { nixpkgs, flake-utils, idr2nix, ... }:
+    # let
+    #   supportedSystems = [ "x86_64-linux" ];
+    # in flake-utils.lib.eachSystem supportedSystems (system:
+    #   let
+    #     pkgs = import nixpkgs {
+    #       inherit system;
+    #     };
+    #   in {
+    #     devShells = {
+    #       default = pkgs.mkShell {
+    #         buildInputs = [ idr2nix.packages."${system}".idr2nix ];
+    #       };
+    #     };
+    #   }
+    # );
+    idr2nix.idris.single {
+      packageName = "hello";
+      sources = builtins.fromJSON (builtins.readFile ./hello.json);
+      ipkg = "hello.ipkg";
+      src = ./.;
+      idris2api = true;
+    };
 }
